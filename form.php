694113d175959eb00b61e2fd980732c6d65def7a
seunogunyemi <timothyogunyemi@gmail.com>
2019-09-16 00:06:54 +0200

    added wallpappers, remember me checkbox, changed logo of the team

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Welcome to Greatness</h1>
+Your name is:<?php
+echo $_POST["name"]; ?><br>
+
+Your Email address is:<?php
+echo $_POST["email address"];
+
+?>
+</body>
+</html>
+
