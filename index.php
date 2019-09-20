ce21941824c23b6772e83809a043d672bc3a0285
Seun Ogunyemi <54277718+seunogunyemi@users.noreply.github.com>
2019-09-20 23:52:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,29 +8,44 @@
 </head>
 <body>
 
-	<div class="framework">
+	<div class="container-fluid">
+				<div class="row">
+						<div class="col-sm-12 col-md-push-3">
+							<center>
+							<img src="team_greatness.png">
+							</center>
+<center>
 		<form action="index.php" method="POST">
-		<img src="team_greatness.png">
-		<input class="name" type="name" name="name" placeholder=" username" required>
-<br>
-		<input class="email" style=" " type="password" name="password" placeholder=" enter password" required>
-<br>
-		<table>
-			<tr>
-				<td class="flush_left">not a member? <a href="#">sign up</a> here</td>
-				<td class="flush_right">forgot password? <a href="#">click here</a></td>
-			</tr>
-		</table>
 
-		<button>LOG IN</a></button>
+			<label>Username</label>
+			<br>
+			<input class="text" type="text" name="name" placeholder="name" required>
+			<br>
 
-		<input style="" type="checkbox" name="remember" checked="checked"> remember me
-		</form>	
-	</div>
+			<label>Password</label>
+			<br>
+			<input class="text" type="password" name="password" placeholder="password" required>
+			<br>
+													<table class="responsive">
+															<tr>
+																<td>not registered?<a href="#">sign up</a></td>
+																<td>forgot login details?<a href="#">click here</a></td>
+															</tr>		
+													</table>
+			
+			<input type="submit" name="submit">
+			<br>
 
+			<input type="checkbox" name="checkbox"> keep me signed in
+			
+	</form>	
+</center>
+						</div>
+				</div>
+			</div>
 
-</body>
-<?php
+	
+	<?php
 				if (isset($_POST['submit'])) {
 					$name = $_POST['name'];
 					$password = $_POST['password'];
@@ -44,4 +59,5 @@
 
 
 	?>
+</body>
 </html>
